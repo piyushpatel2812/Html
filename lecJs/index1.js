ccdce0ba098d1014e7e81e96ae423b73ff18d7de
piyush kumar patel <168159697+piyushpatel2812@users.noreply.github.com>
2025-01-11 19:43:08 +0530

    code

---
+++ lecJs/index1.js
@@ -0,0 +1,39 @@
+// console.log('hello world');
+
+// Number
+// a=5,5
+// String 'c' 'cc' "cc"
+// Boolean true,false ,0,1
+// null
+// undefined
+// Symbol
+// BigInt
+
+// var,let ,const
+
+// var a=5
+// console.log(a);
+// var a=5
+// var a=10
+// console.log(a)
+
+// let a=5
+// a=11
+// console.log(a);
+
+// const a=10
+// console.log(a);
+
+// var a=10
+// var to =a==10
+// console.log(to);
+
+// let a='rahul'
+// let b='jain'
+// console.log(`${a}  ${b}`);
+
+// =  ==   ===
+
+// console.log(false =='false');
+// console.log(5===5);
+console.log('a'===5);
