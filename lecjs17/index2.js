8a28fe696908536909869f26603c7cfc47d1b522
piyush kumar patel <168159697+piyushpatel2812@users.noreply.github.com>
2025-01-14 19:49:25 +0530

    code

---
+++ lecjs17/index2.js
@@ -0,0 +1,49 @@
+// let score=75
+// let attendence=45
+// if(attendence >=75 && score>=80){
+//     console.log("pass")
+// }
+// else if(attendence >=80  || score>=75){
+//     console.log("pass ho gya ")
+// }
+// else{
+//     console.log("fail")
+// }
+
+// // normal fun
+// function sum(a,b){
+//  console.log(a+b);
+// }
+// sum(5,6) // // function calling 
+
+// // aero  function
+// const call=(a,b)=>{
+//     console.log(5+5);
+// }
+// call()
+// call()
+
+// function sum(){
+//     console.log('hello');
+//     return 5
+// }
+// let data=sum()
+// console.log(data);
+// function sum(){
+//     console.log('hello');
+
+// }
+let data=sum()
+console.log(data);
+function
+
+let arr=[1,2,'hello',false,null,undefined]
+for(let i in arr){
+    console.log(i);
+}
+for(let i of arr){
+    console.log(i);
+}
+
+console.log(arr);
+console.log(arr[4]);
